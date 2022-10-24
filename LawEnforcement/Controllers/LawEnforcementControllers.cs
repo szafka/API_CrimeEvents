0c1046405327e8f91baf1e4e3018973cee57714f
Mateusz Arak <mateusz.arak@gmail.com>
2022-10-24 23:35:35 +0200

    Add LawEnforcement Controllers method

--- LawEnforcement/Controllers/LawEnforcementControllers.cs
+++ LawEnforcement/Controllers/LawEnforcementControllers.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LawEnforcement.DTO;
+using LawEnforcement.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace LawEnforcement.Controllers
 {
@@ -6,5 +9,32 @@ namespace LawEnforcement.Controllers
     [ApiController]
     public class LawEnforcementControllers : ControllerBase
     {
+        public readonly ILogger<LawEnforcementControllers> _logger;
+        public readonly ILawEnforcementService _service;
+        public LawEnforcementControllers(ILogger<LawEnforcementControllers> logger, ILawEnforcementService service)
+        {
+            _logger = logger;
+            _service = service;
+        }
+        public async Task<IActionResult> GetAllEnforcementsAsync()
+        {
+            var events = await _service.GetAllAsync();
+            if (events == null)
+            {
+                _logger.LogInformation(NotFound().StatusCode.ToString());
+                return NotFound();
+            }
+            _logger.LogInformation(Ok().StatusCode.ToString());
+            return Ok(events);
+        }
+
+        [SwaggerOperation(Summary = "Add new law enforcement")]
+        [HttpPost]
+        public async Task<IActionResult> AddNewEnforcementAsync(LawEnforcementCreateDTO createDTO)
+        {
+            await _service.AddNewEnforcement(createDTO);
+            _logger.LogInformation(Ok().StatusCode.ToString());
+            return Ok(createDTO);
+        }
     }
 }
